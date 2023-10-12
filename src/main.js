bf837f9d3d1eb93336b1ac5b16f8fc5331b14f5c
Victor Trani <victor.trani@gmail.com>
2023-10-12 17:48:12 -0300

    extracting base website search html

--- src/main.js
+++ src/main.js
@@ -11,3 +11,43 @@
 const axios = require('axios');
 const cheerio = require('cheerio');
 const fs = require('fs');
+
+const olxUrlParams = {
+    baseUrl: 'https://www.olx.com.br/autos-e-pecas/carros-vans-e-utilitarios/',
+    localization: 'estado-rj',
+    minPrixe: 20000,
+    maxPrice: 56000,
+    gnv: {
+        gnvOptional: true,
+        allowGnvOption: false,
+    }
+};
+
+const url_builder = (urlObj) => {
+    let url = `${urlObj.baseUrl}${urlObj.localization}`;
+    if (urlObj.minPrixe && urlObj.maxPrice) {
+        url = url + `?pe=${urlObj.maxPrice}&ps=${urlObj.minPrixe}`;
+    }
+    if (urlObj.gnv.gnvOptional) {
+        url = url + `&hgnv=${urlObj.gnv.allowGnvOption}`;
+    }
+    return url;
+};
+
+const extract_html_content = async (url, params) => {
+    try {
+        const htmlContent = await axios.get(url, params);
+        console.log(htmlContent.data);
+        return htmlContent.data;
+    } catch (err) {
+        console.log('deu ruim na estração do html');
+        console.log(err);
+        return null;
+    }
+};
+
+console.log('URl de Extração: ' + url_builder(olxUrlParams));
+
+
+
+extract_html_content(url_builder(olxUrlParams), {});
