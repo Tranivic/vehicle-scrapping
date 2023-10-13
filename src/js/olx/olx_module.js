54afa354656307c4cae0c403cf9b98aad7d5eebe
Victor Trani <victor.trani@gmail.com>
2023-10-13 03:01:17 -0300

    base cheerio integration added

--- src/js/olx/olx_module.js
+++ src/js/olx/olx_module.js
@@ -1,23 +1,28 @@
 module.exports = {
     urlParams: {
         baseUrl: 'https://www.olx.com.br/autos-e-pecas/carros-vans-e-utilitarios/',
-        localization: 'estado-rj',
-        minPrice: 20000, // Changed "minPrixe" to "minPrice"
-        maxPrice: 56000,
+        localization: 'estado-rj/rio-de-janeiro-e-regiao',
+        minPrice: 10000,
+        maxPrice: 100000,
         gnv: {
-            gnvOptional: true,
+            gnvOptional: false,
             allowGnvOption: false,
         }
     },
 
+    scrapHtmlClasses: {
+        addLink: '.olx-ad-card__link-wrapper'
+    },
+
     url_builder: (urlObj) => {
-        let url = `${urlObj.baseUrl}${urlObj.localization}`;
-        if (urlObj.minPrixe && urlObj.maxPrice) {
-            url = url + `?pe=${urlObj.maxPrice}&ps=${urlObj.minPrixe}`;
+        let url = `${urlObj.baseUrl}${urlObj.localization ? urlObj.localization : ''}`;
+        if (urlObj.minPrice && urlObj.maxPrice) {
+            url = url + `?pe=${urlObj.maxPrice}&ps=${urlObj.minPrice}`;
         }
         if (urlObj.gnv.gnvOptional) {
-            url = url + `&hgnv=${urlObj.gnv.allowGnvOption}`;
+            url = url + `&hgnv=${urlObj.gnv.allowGnvOption}&o=3`;
         }
+        console.log('Url para extração: ' + url)
         return url;
     }
 };
--- src/main.js
+++ src/main.js
@@ -1,24 +1,44 @@
 // URLBASE: https://www.olx.com.br/autos-e-pecas/carros-vans-e-utilitarios/estado-rj?pe=40000&ps=12000&hgnv=true
-// 1º definir a url com todos os parametros necessários.
+// 1º definir a url com todos os parametros necessÃ¡rios.
 // 2º eu vou requisitar o HTMl do site de destino.
 // 3º irei achar os links uteis.
 // 4º irei abrir os links uteis.
-// 5º irei analizar as informações uteis.
-// 6º irei esportar as informações úteis.
+// 5º irei analizar as informaÃ§Ãµes uteis.
+// 6º irei esportar as informaÃ§Ãµes Ãºteis.
 
 
 // Libs import
-// const cheerio = require('cheerio');
-// const fs = require('fs');
+const cheerio = require('cheerio');
 
 
 // Imports
 const extractHtml = require('./js/functionality').extract_html_content;
-const olxModule = require('./js/olx/olx_module')
+const olxModule = require('./js/olx/olx_module');
+
+
+
 
 const runProject = async () => {
-    const mainHtmlContent = await extractHtml(olxModule.url_builder(olxModule.urlParams), {})
-    console.log(mainHtmlContent)
-}
+    const extractionUrl = olxModule.url_builder(olxModule.urlParams);
+    const mainHtmlContent = await extractHtml(extractionUrl, {});
+    let extractedLinks = '';
+
+    if (mainHtmlContent) {
+        console.log('HTML extraido!');
+
+        const $ = cheerio.load(mainHtmlContent);
+        const extractionClass = olxModule.scrapHtmlClasses.addLink;
+
+
+        $(extractionClass).each((i, aTag) => {
+            extractedLinks += ' new-line ' + $(aTag).attr('href') + ' end-line ';
+        });
+
+        console.log(extractedLinks);
+    } else {
+        console.log('Arquivo HTML retornou em branco, verificar o log para detalhes de erro!');
+    }
+};
+
 
 runProject();
