c81c7a2bed246be1b2ce8737c9bd8989e05e9edd
Victor Trani <victor.trani@gmail.com>
2023-10-14 19:30:12 -0300

    fixed code identaion

--- src/js/mixins/fs_functions.js
+++ src/js/mixins/fs_functions.js
@@ -1,7 +1,7 @@
 const fs = require('fs');
 
 exports.save_file = (filePath, fileName, fileData) => {
-    const fullFilePath = filePath + fileName
+    const fullFilePath = filePath + fileName;
 
     fs.writeFile(fullFilePath, fileData, (err) => {
         if (err) {
