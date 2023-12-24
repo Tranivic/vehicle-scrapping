880a3aa639fa7f3c5eeed2f6da70c83ab3115df4
Victor - Notebook <victor.trani1@gmail.com>
2023-12-24 12:01:11 -0300

    create log folder if does not exist

--- src/main.js
+++ src/main.js
@@ -1,15 +1,17 @@
 const olxModule = require('./modules/olx/olx_module');
 const saveFile = require('./js/mixins/fs_functions').save_file;
-
+const createFolder = require('./js/mixins/fs_functions').create_folder
 
 // Run
 async function run(pageNumber) {
     try {
+        createFolder('./log')
+        createFolder('./log/screenshots')
         let storedAds;
-        storedAds = await olxModule.fetchAds(olxModule.url_builder(null, `https://www.olx.com.br/autos-e-pecas/carros-vans-e-utilitarios/estado-rj?ctp=5&ctp=8&me=80000&ms=5000&pe=65000&q=ix35&rs=63&hgnv=false&o=${pageNumber}`), true);
+        // storedAds = await olxModule.fetchAds(olxModule.url_builder(null, `https://www.olx.com.br/autos-e-pecas/carros-vans-e-utilitarios/estado-rj?ctp=5&ctp=8&me=80000&ms=5000&pe=65000&q=ix35&rs=63&hgnv=false&o=${pageNumber}`), true);
         // storedAds = require('../log/adsObj.json');
         // Testing with less results
-        // storedAds = await olxModule.fetchAds(olxModule.url_builder(null, `https://www.olx.com.br/autos-e-pecas/carros-vans-e-utilitarios/estado-rj?ctp=5&ctp=8&me=30000&ms=5000&pe=65000&q=yaris&rs=63&hgnv=false&o=${pageNumber}`), true);
+        storedAds = await olxModule.fetchAds(olxModule.url_builder(null, `https://www.olx.com.br/autos-e-pecas/carros-vans-e-utilitarios/estado-rj?ctp=5&ctp=8&me=30000&ms=5000&pe=65000&q=yaris&rs=63&hgnv=false&o=${pageNumber}`), true);
         saveFile('./log/', `result_page_${pageNumber}.json`, JSON.stringify(storedAds));
     } catch (err) {
         console.log('Run failed: ' + err.message);
