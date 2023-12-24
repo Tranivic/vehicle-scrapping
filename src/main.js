cd8c193c35446bd61bdd6d5f5a9b09ae429de5a4
Tranivic <victor.trani1@gmail.com>
2023-12-23 22:23:53 -0300

    recursive pages run removed

--- src/main.js
+++ src/main.js
@@ -1,45 +1,19 @@
-const fs = require('fs');
 const olxModule = require('./modules/olx/olx_module');
 const saveFile = require('./js/mixins/fs_functions').save_file;
-const rankedAds = require('./js/functionality').ads_ranking;
 
-const pageStart = 1;
-const pageLimit = 1;
 
 // Run
 async function run(pageNumber) {
     try {
         let storedAds;
         storedAds = await olxModule.fetchAds(olxModule.url_builder(null, `https://www.olx.com.br/autos-e-pecas/carros-vans-e-utilitarios/estado-rj?ctp=5&ctp=8&me=80000&ms=5000&pe=65000&q=ix35&rs=63&hgnv=false&o=${pageNumber}`), true);
+        // storedAds = require('../log/adsObj.json');
         // Testing with less results
         // storedAds = await olxModule.fetchAds(olxModule.url_builder(null, `https://www.olx.com.br/autos-e-pecas/carros-vans-e-utilitarios/estado-rj?ctp=5&ctp=8&me=30000&ms=5000&pe=65000&q=yaris&rs=63&hgnv=false&o=${pageNumber}`), true);
-        // storedAds = require('../log/adsObj.json');
-        rankedStoredAds = rankedAds(storedAds);
-        saveFile('./log/', `adsObj.json`, JSON.stringify(storedAds));
-        saveFile('./log/', `result_until_page_${pageNumber}.json`, JSON.stringify(rankedStoredAds));
-
-        pageNumber++;
-        if (pageNumber <= pageLimit) {
-            await run(pageNumber);
-        } else {
-            console.log('Finished!');
-            return;
-        }
+        saveFile('./log/', `result_page_${pageNumber}.json`, JSON.stringify(storedAds));
     } catch (err) {
         console.log('Run failed: ' + err.message);
     }
 }
 
-
-async function recursiveRun() {
-    if (pageStart >= 1 && pageStart <= pageLimit) {
-        for (let page = pageStart; page <= pageLimit; page++) {
-            await run(page);
-        }
-    } else {
-        console.log('Page start or page limit is incorrect!');
-    }
-
-}
-
-recursiveRun();
+run(1);
