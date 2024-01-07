62ae63055dbf698a4a89935230873b6406d87b34
Victor - Notebook <victor.trani1@gmail.com>
2024-01-07 19:12:40 -0300

    fs_fucntions cleaned

--- src/js/mixins/fs_functions.js
+++ src/js/mixins/fs_functions.js
@@ -1,23 +1,22 @@
 const fs = require('fs');
 
 exports.save_file = (filePath, fileName, fileData) => {
-    const fullFilePath = filePath + fileName;
-
-    fs.writeFile(fullFilePath, fileData, (err) => {
-        if (err) {
-            console.error('Error saving the file', err);
-        } else {
-            console.log(`The file ${fileName} was saved!`);
-        }
-    });
+  const fullFilePath = filePath + fileName;
+  fs.writeFile(fullFilePath, fileData, (err) => {
+    if (err) {
+      console.error('Error saving the file', err);
+    } else {
+      console.log(`The file ${fileName} was saved!`);
+    }
+  });
 };
 
-exports.create_folder = (folderName) =>{
-    try {
-        if (!fs.existsSync(folderName)) {
-          fs.mkdirSync(folderName);
-        }
-      } catch (err) {
-        console.error(err);
-      }
-}
+exports.create_folder = (folderName) => {
+  try {
+    if (!fs.existsSync(folderName)) {
+      fs.mkdirSync(folderName);
+    }
+  } catch (err) {
+    console.error(err);
+  }
+};
