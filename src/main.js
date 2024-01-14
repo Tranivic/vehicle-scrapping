7e922bb91d6db107f972d81d88ab347d29085d86
Tranivic <victor.trani1@gmail.com>
2024-01-14 19:33:43 -0300

    better main.js for debbuging

--- src/main.js
+++ src/main.js
@@ -6,12 +6,13 @@ const createFolder = require('@mixins/fs_functions').create_folder;
 // Run
 async function run(pageNumber) {
     try {
+        const searchTerm = 'elantra'
         createFolder('./log');
         let storedAds;
-        storedAds = await olxModule.olxScrapRun(olxModule.url_builder(`https://www.olx.com.br/autos-e-pecas/carros-vans-e-utilitarios/estado-rj?me=80000&ms=5000&pe=65000&q=elantra&rs=63&o=${pageNumber}`), true);
-        saveFile('./log/', `result_page_${pageNumber}.json`, JSON.stringify(storedAds));
+        storedAds = await olxModule.olxScrapRun(olxModule.url_builder(`https://www.olx.com.br/autos-e-pecas/carros-vans-e-utilitarios/estado-rj?me=80000&ms=5000&pe=65000&q=${searchTerm}&rs=63&o=${pageNumber}`), true, true);
+        saveFile('./log/', `result_${searchTerm}_${new Date().valueOf()}.json`, JSON.stringify(storedAds));
     } catch (err) {
-        console.log('Run failed: ' + err.message);
+        console.log('Run failed: ' + err);
     }
 }
 
