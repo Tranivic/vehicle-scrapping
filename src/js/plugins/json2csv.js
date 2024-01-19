290e513ec0d19c0a804c9b794b763568789ab3f3
Tranivic <victor.trani1@gmail.com>
2024-01-18 22:47:48 -0300

    implemented simple csv generation for small files

---
+++ src/js/plugins/json2csv.js
@@ -0,0 +1,12 @@
+const { Parser } = require('@json2csv/plainjs');
+const saveFile = require('@mixins/fs_functions').save_file;
+exports.generate_csv = (array, vehicleName) => {
+    try {
+        console.log(array);
+        const parser = new Parser();
+        const csv = parser.parse(array);
+        saveFile('./csv/', `csv_${vehicleName || "vehicle"}_${new Date().valueOf()}.csv`, csv);
+    } catch (err) {
+        console.log(err);
+    }
+};
