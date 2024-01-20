da422e4055952dc94e8df321329742f330f81419
Tranivic <victor.trani1@gmail.com>
2024-01-19 23:00:25 -0300

    cleaned files and gitignore updated

--- src/js/plugins/json2csv.js
+++ src/js/plugins/json2csv.js
@@ -2,7 +2,6 @@ const { Parser } = require('@json2csv/plainjs');
 const saveFile = require('@mixins/fs_functions').save_file;
 exports.generate_csv = (array, vehicleName) => {
     try {
-        console.log(array);
         const parser = new Parser();
         const csv = parser.parse(array);
         saveFile('./csv/', `csv_${vehicleName || "vehicle"}_${new Date().valueOf()}.csv`, csv);
