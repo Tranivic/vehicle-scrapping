a628998f690edde57d3425450eb5021de53a4f3d
Tranivic <victor.trani1@gmail.com>
2024-04-30 15:40:43 -0300

    adjusted fs functions

--- src/main.js
+++ src/main.js
@@ -1,14 +1,12 @@
 require('module-alias/register');
 const olxModule = require('@modules/olx_module');
 const saveFile = require('@mixins/fs_functions').save_file;
-const createFolder = require('@mixins/fs_functions').create_folder;
 const generateCsv = require("@plugins/json2csv").generate_csv;
 
 // Run
 async function run() {
     try {
         const searchTerm = 'cruze';
-        createFolder('./log');
         let storedAds;
         storedAds = await olxModule.olxScrapRun(olxModule.url_builder(`https://www.olx.com.br/autos-e-pecas/carros-vans-e-utilitarios/estado-rj?f=p&me=80000&ms=5000&pe=65000&q=${searchTerm}&rs=63&o=1`), true, true);
         saveFile('./log/', `result_${searchTerm}_${new Date().valueOf()}.json`, JSON.stringify(storedAds));
