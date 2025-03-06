23305e53e2667f8244b2fd367ed5f5b8f473b2ad
Victor Trani <victor@wdhouse.com.br>
2025-03-05 22:38:32 -0300

    fix main script

--- src/main.js
+++ src/main.js
@@ -6,11 +6,14 @@ const generateCsv = require("@plugins/json2csv").generate_csv;
 // Run
 async function run() {
     try {
-        const searchTerm = 'cruze';
         let storedAds;
-        storedAds = await olxModule.olxScrapRun(olxModule.url_builder(`https://www.olx.com.br/autos-e-pecas/carros-vans-e-utilitarios/estado-rj?f=p&me=80000&ms=5000&pe=65000&q=${searchTerm}&rs=63&o=1`), true, true);
-        saveFile('./log/', `result_${searchTerm}_${new Date().valueOf()}.json`, JSON.stringify(storedAds));
-        generateCsv(storedAds, searchTerm);
+        const url = 'https://www.olx.com.br/autos-e-pecas/carros-vans-e-utilitarios/estado-rj/rio-de-janeiro-e-regiao?me=100000&ms=5000&q=ix35';
+        const urlObject = new URL(url);
+        const searchParams = new URLSearchParams(urlObject.search);
+        const searchTerm = searchParams.get('q');
+        storedAds = await olxModule.olxScrapRun(olxModule.url_builder(url), true, true);
+        saveFile('./log/', `result_${searchTerm || null}_${new Date().valueOf()}.json`, JSON.stringify(storedAds));
+        generateCsv(storedAds,searchTerm);
         console.log('Finished!');
     } catch (err) {
         console.log('Run failed: ' + err);
